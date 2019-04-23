61518c4a03fa7c352b59b67062618ec7897e31ad
JoernL <joern.lindau@t-online.de>
2019-04-24 00:27:52 +0200

    Update MiaomiaoClientManager+UI.swift

--- MiaomiaoClientUI/MiaomiaoClientManager+UI.swift
+++ MiaomiaoClientUI/MiaomiaoClientManager+UI.swift
@@ -5,18 +5,25 @@
 //  Copyright © 2018 LoopKit Authors. All rights reserved.
 //
 
+import LoopKit
 import LoopKitUI
 import HealthKit
 import MiaomiaoClient
 
 
 extension MiaoMiaoClientManager: CGMManagerUI {
-    public static func setupViewController() -> (UIViewController & CGMManagerSetupViewController)? {
-        return MiaomiaoClientSetupViewController()
+    public static func setupViewController() -> (UIViewController & CGMManagerSetupViewController & CompletionNotifying)? {
+        let setupVC = TransmitterSetupViewController.instantiateFromStoryboard()
+            setupVC.cgmManagerType = self
+            return setupVC
+//        return MiaomiaoClientSetupViewController()
     }
 
-    public func settingsViewController(for glucoseUnit: HKUnit) -> (UIViewController) {
-        return MiaomiaoClientSettingsViewController(cgmManager: self, glucoseUnit: glucoseUnit, allowsDeletion: true)
+    public func settingsViewController(for glucoseUnit: HKUnit) -> (UIViewController & CompletionNotifying) {
+        let settings = TransmitterSettingsViewController(cgmManager: self, glucoseUnit: .milligramsPerDeciliter)
+            let nav = SettingsNavigationViewController(rootViewController: settings)
+            return nav
+        //return MiaomiaoClientSettingsViewController(cgmManager: self, glucoseUnit: glucoseUnit, allowsDeletion: true)
     }
 
     public var smallImage: UIImage? {
