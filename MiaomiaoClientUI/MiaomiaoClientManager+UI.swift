ff2a975f1d33190a1fe43aa37367063dbcbe04de
JoernL <joern.lindau@t-online.de>
2019-04-24 12:25:42 +0200

    test

--- MiaomiaoClientUI/MiaomiaoClientManager+UI.swift
+++ MiaomiaoClientUI/MiaomiaoClientManager+UI.swift
@@ -8,22 +8,22 @@
 import LoopKit
 import LoopKitUI
 import HealthKit
+import CGMBLEkit
 import MiaomiaoClient
 
 
 extension MiaoMiaoClientManager: CGMManagerUI {
     public static func setupViewController() -> (UIViewController & CGMManagerSetupViewController & CompletionNotifying)? {
-        let setupVC = MiaomiaoClientSetupViewController.init()
-        
-            return setupVC
+        let setupVC = TransmitterSetupViewController.instantiateFromStoryboard()
+        setupVC.cgmManagerType = self
+        return setupVC
 
         //        return MiaomiaoClientSetupViewController()
     }
 
     public func settingsViewController(for glucoseUnit: HKUnit) -> (UIViewController & CompletionNotifying) {
         let settings = MiaomiaoClientSettingsViewController(cgmManager: self, glucoseUnit: .milligramsPerDeciliter, allowsDeletion: true)
-            let nav = MiaomiaoClientSettingsViewController(rootViewController: settings)
-            return nav
+            return settings
         //return MiaomiaoClientSettingsViewController(cgmManager: self, glucoseUnit: glucoseUnit, allowsDeletion: true)
     }
 
