399953a068016b2f6cebefae1d20ac1a1ab07083
JoernL <joern.lindau@t-online.de>
2019-05-01 00:41:03 +0200

    Update MiaomiaoClientSettingsViewController.swift

--- MiaomiaoClientUI/MiaomiaoClientSettingsViewController.swift
+++ MiaomiaoClientUI/MiaomiaoClientSettingsViewController.swift
@@ -22,7 +22,7 @@ public class MiaomiaoClientSettingsViewController: UITableViewController, Comple
 
     public let allowsDeletion: Bool
     
-    public let extraOffset: String?
+    public let extraOffset = "0"
     
     public init(cgmManager: MiaoMiaoClientManager, glucoseUnit: HKUnit, allowsDeletion: Bool) {
         self.cgmManager = cgmManager
