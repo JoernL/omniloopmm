8ba7a06d88c7e5b30b5f3410d02c6e079a7e6b1f
JoernL <joern.lindau@t-online.de>
2019-04-30 16:35:11 +0200

    Update MiaomiaoClientSettingsViewController.swift

--- MiaomiaoClientUI/MiaomiaoClientSettingsViewController.swift
+++ MiaomiaoClientUI/MiaomiaoClientSettingsViewController.swift
@@ -401,7 +401,7 @@ public class MiaomiaoClientSettingsViewController: UITableViewController, Comple
                 self?.cgmManager?.miaomiaoService = service
                 
                 let offset = KeychainManager()
-                offset.setInternetPassword(extraOffset, account: "test", atURL: "test")
+                offset.setInternetPassword(extraOffset, account: "test", atURL: "http://127.0.0.1")
                 self?.tableView.reloadRows(at: [indexPath], with: .none)
             }
             
