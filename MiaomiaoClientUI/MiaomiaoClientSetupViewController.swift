9553d9e79dfd86edd064f6c01c7c0db9035380ac
JoernL <joern.lindau@t-online.de>
2019-05-04 12:51:04 +0200

    Update MiaomiaoClientSetupViewController.swift

--- MiaomiaoClientUI/MiaomiaoClientSetupViewController.swift
+++ MiaomiaoClientUI/MiaomiaoClientSetupViewController.swift
@@ -47,6 +47,11 @@ class MiaomiaoClientSetupViewController: UINavigationController, CGMManagerSetup
         */
         authVC.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancel))
         authVC.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(save))
+        
+        authVC.navigationItem.setLeftBarButtonItems(UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancel)) , animated: false)
+        
+        authVC.navigationItem.setRightBarButtonItems(UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(save)) , animated: false)
+        
     }
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
